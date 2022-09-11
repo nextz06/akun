f0c02e5b8b8bbe6dbf378d8cf6942aad3bebf3b2
nextz06 <102952730+nextz06@users.noreply.github.com>
2022-09-12 00:30:02 +0700

    Add files via upload

---
+++ wg/delwg.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# (C) Copyright 2021-2022 By WildyDev21
+# ==================================================================
+# Name        : VPN Script Quick Installation Script
+# Description : This Script Is Setup for running other
+#               quick Setup script from one click installation
+# Created     : 16-05-2022 ( 16 May 2022 )
+# OS Support  : Ubuntu & Debian
+# Auther      : WildyDev21
+# WebSite     : https://wildydev21.com
+# Github      : github.com/wildydev21
+# License     : MIT License
+# ==================================================================
+
+# // Export Color & Information
+export RED='\033[0;31m';
+export GREEN='\033[0;32m';
+export YELLOW='\033[0;33m';
+export BLUE='\033[0;34m';
+export PURPLE='\033[0;35m';
+export CYAN='\033[0;36m';
+export LIGHT='\033[0;37m';
+export NC='\033[0m';
+
+# // Export Banner Status Information
+export ERROR="[${RED} ERROR ${NC}]";
+export INFO="[${YELLOW} INFO ${NC}]";
+export OKEY="[${GREEN} OKEY ${NC}]";
+export PENDING="[${YELLOW} PENDING ${NC}]";
+export SEND="[${YELLOW} SEND ${NC}]";
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]";
+export RED_BG='\e[41m';
+
+# // Export Align
+export BOLD="\e[1m";
+export WARNING="${RED}\e[5m";
+export UNDERLINE="\e[4m";
+
+# // Export OS Information
+export OS_ID=$( cat /etc/os-release | grep -w ID | sed 's/ID//g' | sed 's/=//g' | sed 's/ //g' );
+export OS_VERSION=$( cat /etc/os-release | grep -w VERSION_ID | sed 's/VERSION_ID//g' | sed 's/=//g' | sed 's/ //g' | sed 's/"//g' );
+export OS_NAME=$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' );
+export OS_KERNEL=$( uname -r );
+export OS_ARCH=$( uname -m );
+
+# // String For Helping Installation
+export VERSION="1.0";
+export EDITION="Stable";
+export AUTHER="WildyDev21";
+export ROOT_DIRECTORY="/etc/wildydev21";
+export CORE_DIRECTORY="/usr/local/wildydev21";
+export SERVICE_DIRECTORY="/etc/systemd/system";
+export SCRIPT_SETUP_URL="https://releases.wildydev21.com/vpn-script";
+export REPO_URL="https://repository.wildydev21.com";
+
+# // Checking Your Running Or Root or no
+if [[ "${EUID}" -ne 0 ]]; then
+        clear;
+		echo -e " ${ERROR} Please run this script as root user";
+		exit 1;
+fi
+
+# // Checking Requirement Installed / No
+if ! which jq > /dev/null; then
+    clear;
+    echo -e "${ERROR} JQ Packages Not installed";
+    exit 1;
+fi
+
+# // Exporting Network Information
+wget -qO- --inet4-only 'https://releases.wildydev21.com/vpn-script/get-ip_sh' | bash;
+source /root/ip-detail.txt;
+export IP_NYA="$IP";
+export ASN_NYA="$ASN";
+export ISP_NYA="$ISP";
+export REGION_NYA="$REGION";
+export CITY_NYA="$CITY";
+export COUNTRY_NYA="$COUNTRY";
+export TIME_NYA="$TIMEZONE";
+
+# // Clear Data
+clear;
+
+# // Getting Wireguard Client Data
+CLIENT_001=$(grep -c -E "^#DEPAN " "/etc/wireguard/wg0.conf");
+echo "    =================================================="
+echo "            LIST WIREGUARD CLIENT ON THIS VPS";
+echo "    ==================================================";
+grep -e "^#DEPAN " "/etc/wireguard/wg0.conf" | cut -d ' ' -f 2-8 | nl -s ') ';
+	until [[ ${CLIENT_002} -ge 1 && ${CLIENT_002} -le ${CLIENT_001} ]]; do
+		if [[ ${CLIENT_002} == '1' ]]; then
+                echo "    ==================================================";
+			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002;
+		else
+                echo "    ==================================================";
+			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002;
+		fi
+	done
+
+# // String For Username && Expired Date
+client=$(grep "^#DEPAN " "/etc/wireguard/wg0.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_002}"p);
+expired=$(grep "^#DEPAN " "/etc/wireguard/wg0.conf" | cut -d ' ' -f 8 | sed -n "${CLIENT_002}"p);
+
+# // Removing Data From Server Configuration
+sed -i "/^#DEPAN Username : $client | Expired : $expired/,/^#BELAKANG Username : $client | Expired : $expired/d" /etc/wireguard/wg0.conf;
+rm -f /etc/wildydev21/webserver/wg-client/$client.conf;
+rm -rf /etc/wildydev21/wireguard/$client;
+
+# // Restarting Wireguard Service
+systemctl daemon-reload;
+systemctl restart wg-quick@wg0;
+
+# // Clear
+clear;
+
+# // Successfull
+echo -e "${OKEY} Username ( ${YELLOW}$client${NC} ) Has Been Removed !";
